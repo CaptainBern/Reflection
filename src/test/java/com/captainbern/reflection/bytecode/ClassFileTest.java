32f6863460ab35c6c359e5b742cea7a7ea6ab87b
CaptainBern <captainberneby@gmail.com>
2014-04-27 20:51:35 +0200

    Improved unit test to print useful info.

--- src/test/java/com/captainbern/reflection/bytecode/ClassFileTest.java
+++ src/test/java/com/captainbern/reflection/bytecode/ClassFileTest.java
@@ -1,6 +1,8 @@
 package com.captainbern.reflection.bytecode;
 
+import com.captainbern.reflection.bytecode.attribute.Attribute;
 import com.captainbern.reflection.bytecode.exception.ClassFormatException;
+import com.captainbern.reflection.bytecode.field.FieldInfo;
 import com.captainbern.reflection.bytecode.method.MethodInfo;
 import com.captainbern.reflection.utils.ClassUtils;
 import org.junit.Test;
@@ -14,14 +16,31 @@ public class ClassFileTest {
 
     @Test
     public void testBytecode() throws IOException, ClassFormatException {
-        ClassFile classFile = new ClassFile(ClassUtils.classToBytes(ClassFileTest.class.getCanonicalName()));
+        ClassFile classFile = new ClassFile(ClassUtils.classToBytes(ClassFile.class.getCanonicalName()));
         log("Class name: " + classFile.getClassName());
         log("Super class: " + classFile.getSuperClassName());
         log("Access flags: 0x" + Integer.toHexString(classFile.getAccessFlags()));
         log("-------------------------------------------------------");
 
-        for(MethodInfo info : classFile.getMethods()) {
-            log(info.getSignature());
+        log("Methods:");
+        for(MethodInfo method : classFile.getMethods()) {
+            log("\t" + method.getName() + " - " + method.getSignature());
+            for(Attribute attribute : method.getAttributes()) {
+                log("\t\t" + attribute.getName());
+            }
+        }
+
+        log("Fields:");
+        for(FieldInfo field : classFile.getFields()) {
+            log("\t" + field.getName() + " - " + field.getSignature());
+            for(Attribute attribute : field.getAttributes()) {
+                log("\t\t" + attribute.getName());
+            }
+        }
+
+        log("Attributes:");
+        for(Attribute attribute : classFile.getAttributes()) {
+            log("\t" + attribute.getName());
         }
     }
 

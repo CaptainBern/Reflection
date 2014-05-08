ee068cb2bf03ae7e2951a723095c541cfa253da1
CaptainBern <captainberneby@gmail.com>
2014-05-08 15:59:10 +0200

    fixed some small issues.

--- Reflection/src/main/java/com/captainbern/reflection/ReflectionUtils.java
+++ Reflection/src/main/java/com/captainbern/reflection/ReflectionUtils.java
@@ -3,7 +3,11 @@ package com.captainbern.reflection;
 import com.captainbern.reflection.matcher.Matcher;
 import com.captainbern.reflection.matcher.Matchers;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -15,7 +19,7 @@ public class ReflectionUtils {
         Set<Class<?>> result = new HashSet<>();
         if(clazz != null && (INCLUDE_OBJECT || !clazz.equals(Object.class))) {
             result.add(clazz);
-            result.addAll(getAllSuperClasses(clazz));
+            result.addAll(getAllSuperClasses(clazz.getSuperclass()));
             for(Class<?> iface : clazz.getInterfaces()) {
                 result.addAll(getAllSuperClasses(iface));
             }
@@ -23,8 +27,74 @@ public class ReflectionUtils {
         return result;
     }
 
-    public static Set<Class<?>> getAllSuperClasses(final Class<?> clazz, Matcher<? super Class<?>> matchers) {
+    public static Set<Class<?>> getAllSuperClasses(final Class<?> clazz, Matcher<? super Class<?>>... matchers) {
         return match(getAllSuperClasses(clazz), matchers);
+    }
+
+    public static Set<Field> getFields(final Class<?> clazz) {
+        Set<Field> result = new HashSet<>();
+        Collections.addAll(result, clazz.getDeclaredFields());
+        return null;
+    }
+
+    public static Set<Field> getAllFields(final Class<?> clazz) {
+        Set<Field> result = new HashSet<>();
+        for(Class<?> superClass : getAllSuperClasses(clazz)) {
+            result.addAll(getFields(superClass));
+        }
+        return result;
+    }
+
+    public static Set<Field> getFields(final Class<?> clazz, Matcher<? super Field>... matchers) {
+        return match(getFields(clazz), matchers);
+    }
+
+    public static Set<Field> getAllFields(final Class<?> clazz, Matcher<? super Field>... matchers) {
+        return match(getAllFields(clazz), matchers);
+    }
+
+    public static Set<Constructor> getConstructors(final Class<?> clazz) {
+        Set<Constructor> result = new HashSet<>();
+        Collections.addAll(result, clazz.getDeclaredConstructors());
+        return result;
+    }
+
+    public static Set<Constructor> getAllConstructors(final Class<?> clazz) {
+        Set<Constructor> result = new HashSet<>();
+        for(Class<?> superClass : getAllSuperClasses(clazz)) {
+            result.addAll(getConstructors(superClass));
+        }
+        return result;
+    }
+
+    public static Set<Constructor> getConstructors(final Class<?> clazz, Matcher<? super Constructor>... matchers) {
+        return match(getConstructors(clazz), matchers);
+    }
+
+    public static Set<Constructor> getAllConstructors(final Class<?> clazz, Matcher<? super Constructor>... matchers) {
+        return match(getAllConstructors(clazz), matchers);
+    }
+
+    public static Set<Method> getMethods(final Class<?> clazz) {
+        Set<Method> result = new HashSet<>();
+        Collections.addAll(result, clazz.getDeclaredMethods());
+        return result;
+    }
+
+    public static Set<Method> getAllMethods(final Class<?> clazz) {
+        Set<Method> result = new HashSet<>();
+        for(Class<?> superClass : getAllSuperClasses(clazz)) {
+            result.addAll(getMethods(superClass));
+        }
+        return result;
+    }
+
+    public static Set<Method> getMethods(final Class<?> clazz, Matcher<? super Method>... matchers) {
+        return match(getMethods(clazz), matchers);
+    }
+
+    public static Set<Method> getAllMethods(final Class<?> clazz, Matcher<? super Method>... matchers) {
+        return match(getAllMethods(clazz), matchers);
     }
 
     public static <T> Set<T> match(final Set<T> classes, final Matcher<? super T>... matchers) {
@@ -34,9 +104,9 @@ public class ReflectionUtils {
             Set<T> elements = new HashSet<>();
             Matcher<T> combinedMatcher = Matchers.combine(Arrays.asList(matchers));
             for(T clazz : classes) {
-                 if(combinedMatcher.matches(clazz)) {
-                     elements.add(clazz);
-                 }
+                if(combinedMatcher.matches(clazz)) {
+                    elements.add(clazz);
+                }
             }
             return elements;
         }

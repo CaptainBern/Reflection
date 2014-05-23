9ab436fd17fb2cc6a981f755e8375b443d220372
CaptainBern <captainberneby@gmail.com>
2014-05-23 15:38:30 +0200

    Hijacking Boolean

--- Reflection/src/test/java/com/captainbern/reflection/ReflectionTest.java
+++ Reflection/src/test/java/com/captainbern/reflection/ReflectionTest.java
@@ -2,10 +2,22 @@ package com.captainbern.reflection;
 
 import org.junit.Test;
 
+import java.lang.reflect.Modifier;
+
+import static org.junit.Assert.assertTrue;
+
 public class ReflectionTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void testIllegalArgumentNull() {
         new Reflection().reflect((Class) null);
+    }
+
+    @Test
+    public void testLetsHackBoolean() {
+        SafeField<Boolean> FALSE =  new Reflection().reflect(Boolean.class).getSafeFieldByName("FALSE");
+        FALSE.setModifiers(Modifier.PUBLIC | Modifier.STATIC);
+        FALSE.getAccessor().set(null, true);
+        assertTrue(Boolean.FALSE);
     }
 }

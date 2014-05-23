54ce826461b40195c3b892caf71bddbd68880c03
CaptainBern <captainberneby@gmail.com>
2014-05-23 19:01:40 +0200

    Stackmaps are a bitch

---
+++ JBEL/src/main/java/com/captainbern/jbel/generator/Frame.java
@@ -0,0 +1,12 @@
+package com.captainbern.jbel.generator;
+
+public class Frame {
+
+    /**
+     * Creating/generating a frame is hard. It's basically the only
+     * piece of quality code in a java class (in it's bytecode representation at least)
+     *
+     * One can easily create a class file because it's really simple to do,
+     * but since they introduced stackmaps and frames etc.. this has become much harder.
+     */
+}

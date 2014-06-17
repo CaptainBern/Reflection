57e29df0169ffb15d0a636fc3b3734a4d654afe9
CaptainBern <captainberneby@gmail.com>
2014-06-17 13:04:11 +0200

    Added example converter

---
+++ Minecraft-Reflection/src/main/java/com/captainbern/minecraft/conversion/BukkitConverters.java
@@ -0,0 +1,28 @@
+package com.captainbern.minecraft.conversion;
+
+import com.captainbern.minecraft.reflection.MinecraftReflection;
+import com.captainbern.minecraft.wrapper.WrappedDataWatcher;
+
+public class BukkitConverters {
+
+    public static Converter<WrappedDataWatcher> getDataWatcherConverter() {
+        return new Converter<WrappedDataWatcher>() {
+            @Override
+            public WrappedDataWatcher getWrapped(Object object) {
+                if (MinecraftReflection.getDataWatcherClass().isAssignableFrom(object.getClass())) {
+                    return new WrappedDataWatcher(object);
+                } else if (object instanceof WrappedDataWatcher) {
+                    return (WrappedDataWatcher) object;
+                } else {
+                    throw new IllegalArgumentException("Invalid type: " + object);
+                }
+            }
+
+            @Override
+            public Object getUnWrapped(Class<?> type, WrappedDataWatcher wrapperType) {
+                return wrapperType.getHandle();
+            }
+        };
+    }
+
+}
